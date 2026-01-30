fd1ee81f1c23afe3ec5e080e5ddcc758bf9c6147
Reem Alhasan <144840695+ReemAlhasan@users.noreply.github.com>
2026-01-30 17:00:11 +0100

    Add displayVatten function to render image

---
+++ vatten.py
@@ -0,0 +1,8 @@
+import pygame
+
+def displayVatten(bakgrund):
+    playerImg = pygame.image.load('C:/Users/reema/Documents/KlimatStudio/assets/vatten.png')
+    vatten_x = 200 
+    vatten_y = 200
+    vatten_change = 0
+    bakgrund.blit(playerImg, (vatten_x, vatten_y))
